40ce72004eb25849c912ca642708ebb8d4efbc8a
Matthew Beal <bealmatthewthomas@gmail.com>
2021-09-21 12:46:50 -0400

    we fumblin around

--- pages/rooms/[rId].tsx
+++ pages/rooms/[rId].tsx
@@ -1,14 +1,86 @@
 import { useRouter } from 'next/router'
+import React, {useEffect} from 'react'
 
 
 const Post = ({socket}) => {
-    console.log(socket)
+    // console.log(socket)
   const router = useRouter()
   const { rId } = router.query
 
+  type CurrentState = {
+    admin: string | null;
+    points: object;
+    showPoints: boolean;
+  };
+
+  const pointValues = [0, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+
+  const[currentState, setCurrentState] = React.useState({
+    admin: null,
+    points: {},
+    showPoints: false,
+  })
+  
+  // $: assignedPointsArray =
+  //   Object.keys(currentState?.points || {})
+  //     .filter((userId) => userId != socket.id)
+  //     .map((userId) => {
+  //       return currentState.points[userId];
+  //     })
+  //     .sort() || [];
+
+  
+
+
+
+
+  // $: userAssignedPointValue = currentState?.points[socket.id];
+
+
+  // $: isAdmin = currentState?.admin === socket.id;
+
+  //does socket even work as a useEffect dependency?
+  useEffect(() => {
+    console.log(socket.id, currentState?.admin)
+    setCurrentState((currentState) =>  ({...currentState, admin: currentState?.admin === socket.id}))
+  },[socket])
+
+  console.log(currentState)
+
+  // function assignPointValue(pointValue: number) {
+  //   if (currentState.showPoints === true) return;
+  //   currentState.points[socket.id] = pointValue;
+  //   socket.emit("assign-point-value", rId, pointValue, (res) => {
+  //     console.error("Error assigning point value", res);
+  //     currentState.points[socket.id] = null;
+  //   });
+  // }
+
+  // function adminShowPoints() {
+  //   socket.emit("admin-show-points", rId);
+  // }
+
+  // function adminResetPoints() {
+  //   socket.emit("admin-reset-points", rId);
+  // }
+
+  // React.useEffect( () =>
+  //   socket.emit("join-room-by-id", rId, (res) => {
+  //     if (res.status === "error") {
+  //       navigate("/");
+  //     }
+
+  //     currentState = res.data;
+  //   })
+
+  //   socket.on("state-change", (state) => {
+  //     currentState = state;
+  //   })
+  // ), []);
+
   return (
     <div className="page-layout">
-    <div className="cards-container">
+    {/* <div className="cards-container">
       <div className="pointing-cards">
         <div
           className={`flip-container${
@@ -46,17 +118,17 @@ const Post = ({socket}) => {
         {/each}
       </div>
     </div>
-  
-    {#if isAdmin}
+   */}
+    {currentState.admin &&
       <div className="admin-controls">
-        <button className="admin-button" on:click={adminShowPoints}
+        <button className="admin-button"
           >Display points</button
         >
-        <button className="admin-button" on:click={adminResetPoints}
+        <button className="admin-button" 
           >Reset points</button
         >
       </div>
-    {/if}
+    }
   </div>
   )
 }
