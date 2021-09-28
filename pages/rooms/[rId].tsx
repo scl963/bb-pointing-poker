415e158d2987d8f798015feec65b5aee22296135
Erin Hastings <erin.hastings@butcherbox.com>
2021-09-28 11:44:31 -0500

    React Svelte Room logic to React

--- pages/rooms/[rId].tsx
+++ pages/rooms/[rId].tsx
@@ -15,72 +15,59 @@ const Post = ({socket}) => {
 
   const pointValues = [0, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
 
-  const[currentState, setCurrentState] = React.useState({
+  const[currentState, setCurrentState] = React.useState<CurrentState>({
     admin: null,
     points: {},
     showPoints: false,
   })
   
-  // $: assignedPointsArray =
-  //   Object.keys(currentState?.points || {})
-  //     .filter((userId) => userId != socket.id)
-  //     .map((userId) => {
-  //       return currentState.points[userId];
-  //     })
-  //     .sort() || [];
-
-  
+  const assignedPointsArray = React.useMemo(() => { 
+    return Object.keys(currentState?.points || {})
+    .filter((userId) => userId != socket.id)
+    .map((userId) => {
+      return currentState.points[userId];
+    })
+    .sort() || []}, [currentState?.points, socket.id])
 
 
-
-
-  // $: userAssignedPointValue = currentState?.points[socket.id];
-
-
-  // $: isAdmin = currentState?.admin === socket.id;
-
-  //does socket even work as a useEffect dependency?
   useEffect(() => {
-    console.log(socket.id, currentState?.admin)
-    setCurrentState((currentState) =>  ({...currentState, admin: currentState?.admin === socket.id}))
-  },[socket])
+    socket.emit("join-room-by-id", rId, (res) => {
+      if (res.status === "error") {
+        router.push('/')
+      }
+
+      setCurrentState({...res.data})
+    });
+
+    socket.on("state-change", (state) => {
+      setCurrentState({...state})
+    });
+  }, [socket]);
+  
+  const userAssignedPointValue = currentState?.points?.[socket.id];
+  const isAdmin = currentState.admin
 
   console.log(currentState)
 
-  // function assignPointValue(pointValue: number) {
-  //   if (currentState.showPoints === true) return;
-  //   currentState.points[socket.id] = pointValue;
-  //   socket.emit("assign-point-value", rId, pointValue, (res) => {
-  //     console.error("Error assigning point value", res);
-  //     currentState.points[socket.id] = null;
-  //   });
-  // }
+  function assignPointValue(pointValue: number) {
+    if (currentState.showPoints === true) return;
+    currentState.points[socket.id] = pointValue;
+    socket.emit("assign-point-value", rId, pointValue, (res) => {
+      console.error("Error assigning point value", res);
+      currentState.points[socket.id] = null;
+    });
+  }
 
-  // function adminShowPoints() {
-  //   socket.emit("admin-show-points", rId);
-  // }
+  function adminShowPoints() {
+    socket.emit("admin-show-points", rId);
+  }
 
-  // function adminResetPoints() {
-  //   socket.emit("admin-reset-points", rId);
-  // }
-
-  // React.useEffect( () =>
-  //   socket.emit("join-room-by-id", rId, (res) => {
-  //     if (res.status === "error") {
-  //       navigate("/");
-  //     }
-
-  //     currentState = res.data;
-  //   })
-
-  //   socket.on("state-change", (state) => {
-  //     currentState = state;
-  //   })
-  // ), []);
-
+  function adminResetPoints() {
+    socket.emit("admin-reset-points", rId);
+  }
   return (
     <div className="page-layout">
-    {/* <div className="cards-container">
+     <div className="cards-container">
       <div className="pointing-cards">
         <div
           className={`flip-container${
@@ -94,39 +81,29 @@ const Post = ({socket}) => {
             </div>
           </div>
         </div>
-        {#each assignedPointsArray as assignedPointValue}
-          <div className="point-placeholder">
-            {#if assignedPointValue === null}
-              ...
-            {:else if assignedPointValue && !currentState.showPoints}
-              ?
-            {:else}
-              {assignedPointValue}
-            {/if}
-          </div>
-        {/each}
+        </div>
+        {assignedPointsArray.map((assignedPointValue, index) => (
+          <div key={index} className="point-placeholder">
+          {assignedPointValue == null ? '...' : assignedPointValue}
+        </div>
+        ))}
       </div>
-  
       <div className="pointing-cards">
-        {#each pointValues as pointValue}
+        {pointValues.map((point) => (
           <button
-            className="point-button"
-            on:click={() => assignPointValue(pointValue)}
-          >
-            {pointValue}
-          </button>
-        {/each}
+          key={point}
+          className="point-button"
+          onClick={() => assignPointValue(point)}
+        >
+          {point}
+        </button>
+        ))}
       </div>
-    </div>
-   */}
-    {currentState.admin &&
+    {isAdmin &&
       <div className="admin-controls">
-        <button className="admin-button"
-          >Display points</button
-        >
-        <button className="admin-button" 
-          >Reset points</button
-        >
+        <button className="admin-button" onClick={adminShowPoints}
+        >Display points</button>
+        <button className="admin-button" onClick={adminResetPoints}>Reset points</button>
       </div>
     }
   </div>
